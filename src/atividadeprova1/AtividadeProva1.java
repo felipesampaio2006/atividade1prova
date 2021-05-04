5108669f9907550a67ce7cc546a625a123c35abf
felipe Sampaio <samlipe2006@gmail.com>
2021-05-04 20:49:18 -0300

    projeto criado dia 04/05/2021 - atividade 1 da prova

---
+++ src/atividadeprova1/AtividadeProva1.java
@@ -0,0 +1,27 @@
+package atividadeprova1;
+
+import static java.lang.Math.PI;
+import java.util.Scanner;
+/**
+ *
+ * @author Daniela
+ */
+public class AtividadeProva1 {
+
+    private static double raio2;
+    public static void main(String[] args, double diametroCirculo) {
+   //Faça um algoritmo que receba o valor do raio de um circulo qualquer. Calcule 
+  // o perímetro do círculo (P = 2.pi.r) , a Área do circulo (A = PI . r²) e o Diâmetro 
+  // do círculo (D = 2 . r). Mostre as respostas. obs.: Para testar utilize o NetBeans, 
+ //  em seguida na resposta coloque o link do GitHub com sua resposta.//
+       
+        double perim, area, diametroCiruclo, raio = 0;
+        Scanner ler = new Scanner(System.in);
+        
+        perim= 2*PI*raio;
+        area= PI*raio2;
+        diametroCirculo = 2*raio2;
+        
+    }
+    
+}
